e294f0eb5f41f50485cdd484f38839a2677a0dcc
Chris Digirolamo <chrisdigirolamo@gmail.com>
2016-03-10 00:13:05 -0500

    Initial commit of semi-working code

---
+++ piltesseract.py
@@ -0,0 +1,88 @@
+"""The piltesseract module is a tesseract >=3.03 wrapper.
+
+This module allows quick conversion of PIL Image instances to text using
+Tesseract-OCR.
+
+Example:
+
+Attributes:
+    tesseract_dir_path (unicode): The path to the tesseract install directory.
+    tesseract_stderr (): Where to pipe tesseract stderr stream. Defaults to
+        sys.stdin. If None, errors are ignored.
+
+"""
+import os
+import subprocess
+import sys
+from PIL import Image
+
+#If tesseract binary is not on os.environ["PATH"] the put the path below.
+tesseract_dir_path = ""
+tesseract_stderr = sys.stdout
+
+
+#variables for finding and setting up the paths for the tesseract binary
+_tesseract_bin_path = 'tesseract'
+_tess_cwd = None
+_tess_env = None
+if tesseract_dir_path:
+    _tesseract_bin_path = os.path.join(tesseract_dir_path, 'tesseract')
+    _tess_cwd = tesseract_dir_path
+    _tess_env = {'path': tesseract_dir_path}
+#So console window does not popup
+_console_startup_info = subprocess.STARTUPINFO()
+_console_startup_info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+_console_startup_info.wShowWindow = subprocess.SW_HIDE
+
+
+def get_text_from_image(image, psm=3, lang="eng", tessdata_dir_path=None,
+                        user_words_path=None, user_patterns_path=None,
+                        config_name=None, **config_variables):
+    """Uses tesseract to get single line from an image
+    
+    The arguments mirror the official command line's usage. See
+    https://tesseract-ocr.googlecode.com/svn/trunk/doc/tesseract.1.html
+
+    Args:
+        image (Image): The image to find text from.
+        psm (int): Page Segmentation Mode. Limits Tesseracts layout
+            analysis (see the Tesseract docs). Default is full page analysis.
+        lang (unicode): The language to use. Default is English.
+        tessdata_dir_path (unicode): The path to
+        tessdata_dir_path (unicode): The path to
+        tessdata_dir_path (unicode): The path to
+        **config_variables: The config variables for tesseract.
+
+    Returns:
+        unicode: The parsed text.
+
+    """
+    bin_path = _tesseract_bin_path
+    commands = ["{} stdin stdout -psm {} -l {}".format(bin_path, psm, lang)]
+    if tessdata_dir_path is not None:
+        commands.append('--tessdata-dir"{}"'.format(tessdata_dir_path))
+    if user_words_path is not None:
+        commands.append('--user-words"{}"'.format(user_words_path))
+    if user_patterns_path is not None:
+        commands.append('--user-patterns"{}"'.format(user_patterns_path))
+    for config_var, value in config_variables.items():
+        commands.append('-c {}={}'.format(config_var, value))
+    if config_name is not None:
+        commands.append(config_name)
+    command = ' '.join(commands)
+    print 'command: ', command
+    pipe = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        cwd=_tess_cwd,
+        env=_tess_env,
+        startupinfo=_console_startup_info,
+        )
+    image.save(pipe.stdin, format='bmp')
+    pipe.stdin.close()
+    text = pipe.stdout.read()
+    error = pipe.stderr.read()
+    if error and tesseract_stderr is not None:
+        tesseract_stderr.write(error)
+    text =  unicode(text, "utf-8", errors="ignore")
+    return text
